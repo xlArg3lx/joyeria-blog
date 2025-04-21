d4d5e318dbe608cdeda2fa25335de34b86cc37be
Argenedis Alvarez <socialn27@gmail.com>
2025-04-21 15:16:58 -0500

    Modified captain & vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,42 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
         react(),
     ],
     server: {
         hmr: {
-            host: 'joyeria-elegance.pixflow.me',
-            protocol: 'wss' // Usar WebSocket seguro
-        }
+            host: "joyeria-elegance.pixflow.me",
+            protocol: "wss", // Usar WebSocket seguro
+        },
     },
     build: {
-        outDir: 'public/build',
+        outDir: "public/build",
         manifest: true,
         rollupOptions: {
             output: {
-                assetFileNames: 'assets/[name]-[hash][extname]',
-                entryFileNames: 'assets/[name]-[hash].js',
-            }
-        }
+                // Forzar la generación en /public/build/ (no en subdirectorio .vite)
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`,
+            },
+        },
     },
-    base: '/build/', // Ruta base relativa para producción
+    base: "/build/", // Ruta base relativa para producción
     optimizeDeps: {
-        include: ['react', 'react-dom']
+        include: ["react", "react-dom"],
     },
     resolve: {
         alias: {
-            '@': '/resources/js',
-            '~': '/resources',
-            'public': '/public',
-        }
-    }
+            "@": "/resources/js",
+            "~": "/resources",
+            public: "/public",
+        },
+    },
 });
