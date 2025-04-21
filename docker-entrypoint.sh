7ed85c469145eb8117a73c46bff2ebcc8a18d5a7
Argenedis Alvarez <socialn27@gmail.com>
2025-04-21 10:53:53 -0500

    Add entrypoint.sh and modified captain

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Activar el modo de mantenimiento si es necesario
+# php artisan down --no-interaction
+
+# Optimizaciones de caché
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Corregir permisos de almacenamiento
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Desactivar el modo de mantenimiento
+# php artisan up --no-interaction
+
+# Ejecutar Apache en primer plano
+exec "$@"
