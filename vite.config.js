f9d2601f98a3c74a8e66c835bd6d93a94f851372
Argenedis Alvarez <socialn27@gmail.com>
2025-04-21 21:39:50 -0500

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,22 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
+            buildDirectory: 'build', // Esto es lo importante
         }),
         react(),
     ],
     server: {
         hmr: {
             host: "joyeria-elegance.pixflow.me",
-            protocol: "wss", // Usar WebSocket seguro
+            protocol: "wss",
         },
     },
-    build: {
-        outDir: "public/build",
-        manifest: true,
-        rollupOptions: {
-            output: {
-                // Forzar la generación en /public/build/ (no en subdirectorio .vite)
-                entryFileNames: `assets/[name].js`,
-                chunkFileNames: `assets/[name].js`,
-                assetFileNames: `assets/[name].[ext]`,
-            },
-        },
-    },
-    base: "/build/", // Ruta base relativa para producción
     optimizeDeps: {
         include: ["react", "react-dom"],
     },
